bc570df6f000f0432ff6f262562478feee1bd558
rcyq <quan0203@gmail.com>
2015-04-20 20:22:13 +0800

    Increased minimum scale for minimap

--- public/src/scenes.js
+++ public/src/scenes.js
@@ -717,7 +717,7 @@ Q.scene(SCENE_LEVEL, function(stage) {
       var cLayer = mapStage._collisionLayers[0];
       var vpScale = 0.1;
       if(cLayer) {
-        vpScale = Math.max(0.1, Math.min(1, Math.max(screenW/cLayer.p.w, screenH/cLayer.p.h)));
+        vpScale = Math.max(0.2, Math.min(1, Math.max(screenW/cLayer.p.w, screenH/cLayer.p.h)));
         vpScale = Math.round(vpScale * 100) / 100;
       }
       
